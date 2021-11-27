b2624553b207570b8d34b0384a2877dd8024d198
Ptrnd <putranda.bagus@gmail.com>
2021-11-27 11:41:38 +0700

    create transaksi

--- controller/transaksiController.js
+++ controller/transaksiController.js
@@ -1,3 +1,4 @@
+const { create } = require('lodash');
 const { Transaksi } = require('../models')
 
 module.exports = {
@@ -15,12 +16,36 @@ module.exports = {
         res.send(transaksi)
     },
 
-    async create(req, res) {
-        const result = await Transaksi.create({
-            'userid': req.body.userid
-        })
+    // async create(req, res) {
+    //     const result = await Transaksi.create({
+    //         'userid': req.body.userid
+    //     })
 
-        res.send(result)
+    //     res.send(result)
+    // },
+
+    async create(req, res) {
+        try {
+            const result = await Transaksi.create({
+                'userid': req.body.userid}).then(res => {
+                    id = req.body.transaksi_id
+                    result = res
+                    result.menu = []
+                    req.body.menu.forEach(data => {
+                        await TransaksiDetail.create({
+                            'transaksi_id': id,
+                            'menu_id': data.id,
+                            'jumlah': data.jumlah
+                        }).then(tdres => {
+                            result.menu.push(tdres)
+                    })
+                })
+            }).finally(() => {
+                res.send(result)
+            })
+        } catch (error) {
+            console.log(error)
+        }
     },
 
     async update(req, res) {
